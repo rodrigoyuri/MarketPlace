ededaf75f1f0e0328f53bac4d4259b3b7b8c3002
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-26 19:10:04 -0300

    Validação de inputs para Store feitos.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -11,36 +11,51 @@
 
     <div class="form-group">
         <label for="">Nome do Produto</label>
-        <input type="text" name="name" class="form-control">
+        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+
+        @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Descrição</label>
-        <input type="text" name="description" class="form-control">
+        <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+
+        @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Conteúdo</label>
-        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+        <textarea name="body" id="body" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror"></textarea>
+
+        @error('body')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Preço</label>
-        <input type="text" name="price" class="form-control">
+        <input type="text" name="price" class="form-control @error('price') is-invalid @enderror">
+
+        @error('price')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Slug</label>
         <input type="text" name="slug" class="form-control">
-    </div>
-
-    <div class="form-group">
-        <label for="">Lojas</label>
-        <select name="store" class="form-control">
-            @foreach($stores as $store)
-                <option value="{{$store->id}}">{{$store->name}}</option>
-            @endforeach
-        </select>
     </div>
 
     <div>
--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -11,22 +11,46 @@
 
     <div class="form-group">
         <label for="">Nome da Loja</label>
-        <input type="text" name="name" class="form-control">
+        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+
+        @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Descrição</label>
-        <input type="text" name="description" class="form-control">
+        <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+
+        @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Telefone</label>
-        <input type="text" name="phone" class="form-control">
+        <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
+
+        @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">
         <label for="">Celular/WhatsApp</label>
-        <input type="text" name="mobile_phone" class="form-control">
+        <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
+
+        @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div class="form-group">

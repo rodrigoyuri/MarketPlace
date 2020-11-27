10baed46767fb16c6ca576e8fc934132dde7b33d
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-27 17:00:13 -0300

    Validação em index de Store.

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -5,36 +5,34 @@
 
 @if(!$store)
     <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+@else
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Loja</td>
+                <td>Total de Produtos</td>
+                <td>Ações</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td>{{$store->products()->count()}}</td>
+                <td>
+                    <div class="btn-group">
+                    <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
+                    <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
+                    </form>
+                    </div>
+                </td>
+            </tr>        
+        </tbody>
+    </table>
 @endif
-
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <td>#</td>
-            <td>Loja</td>
-            <td>Total de Produtos</td>
-            <td>Ações</td>
-        </tr>
-    </thead>
-    <tbody>
-            @if($store)
-                <tr>
-                    <td>{{$store->id}}</td>
-                    <td>{{$store->name}}</td>
-                    <td>{{$store->products()->count()}}</td>
-                    <td>
-                        <div class="btn-group">
-                        <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
-                        <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="POST">
-                            @csrf
-                            @method("DELETE")
-                            <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
-                        </form>
-                        </div>
-                    </td>
-                </tr>
-            @endif
-    </tbody>
-</table>
 
 @endsection

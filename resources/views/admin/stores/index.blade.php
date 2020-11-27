c55d3f710502753a4697eb61d163b9f423236d1b
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-26 21:23:51 -0300

    Ajuste para acesso de index Store.

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -17,21 +17,23 @@
         </tr>
     </thead>
     <tbody>
-            <tr>
-                <td>{{$store->id}}</td>
-                <td>{{$store->name}}</td>
-                <td>{{$store->products()->count()}}</td>
-                <td>
-                    <div class="btn-group">
-                    <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
-                    <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="POST">
-                        @csrf
-                        @method("DELETE")
-                        <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
-                    </form>
-                    </div>
-                </td>
-            </tr>
+            @if($store)
+                <tr>
+                    <td>{{$store->id}}</td>
+                    <td>{{$store->name}}</td>
+                    <td>{{$store->products()->count()}}</td>
+                    <td>
+                        <div class="btn-group">
+                        <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">EDITAR</a>
+                        <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
+                        </form>
+                        </div>
+                    </td>
+                </tr>
+            @endif
     </tbody>
 </table>
 

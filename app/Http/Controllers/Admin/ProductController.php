62b05c278c2474e8852731408e920360c8192e0b
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-27 17:16:06 -0300

    Instalação do pacote para slug

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -53,11 +53,12 @@ class ProductController extends Controller
     public function store(ProductRequest $request)
     {
         $data = $request->all();
+        $categories = $request->get('categories', null);
 
         $store = auth()->user()->store;
         $product = $store->products()->create($data);
 
-        $product->categories()->sync($data['categories']);
+        $product->categories()->sync($categories);
 
         if($request->hasFile('photos')) {
             $images = $this->imageUpload($request->file('photos'), 'image');
@@ -103,10 +104,15 @@ class ProductController extends Controller
     public function update(ProductRequest $request, $id)
     {
         $data = $request->all();
+        $categories = $request->get('categories', null);
 
         $product = $this->product->find($id);
         $product->update($data);
-        $product->categories()->sync($data['categories']);
+        
+
+        if(!is_null($categories)) {
+            $product->categories()->sync($categories);
+        }
 
         if($request->hasFile('photos')) {
             $images = $this->imageUpload($request->file('photos'), 'image');

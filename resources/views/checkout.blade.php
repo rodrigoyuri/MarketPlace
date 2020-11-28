c8db814fd1bb661955e960d5c88bd6e643fc2912
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-28 20:49:31 -0300

    Checkout transparente de pagamento concluido e tela de obrigado ao terminar a compra criada.

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.front');
 
+@section('stylesheets')
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+@endsection
+
 @section('content')
 
 <div class="container">
@@ -11,17 +15,27 @@
             </div>
         </div>          
         <form action="" method="POST">
+            
             <div class="row">
                 <div class="col-md-12">
-                    <label for="">Número do cartão</label>
-                    <input type="text" name="card-number" id="" class="form-control">
+                    <label for="">Nome no cartão</label>
+                    <input type="text" name="card_name" id="" class="form-control">
+                </div>
+            </div>
+        
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="">Número do cartão <span class="brand"></span></label>
+                    <input type="text" name="card_number" id="" class="form-control">
+                    <input type="hidden" name="card_brand">
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-md-4 form-group">
                     <label for="">Mês de Expiração</label>
-                    <input type="text" name="car_month" id="" class="form-control">
+                    <input type="text" name="card_month" id="" class="form-control">
                 </div>
 
                 <div class="col-md-4 form-group">
@@ -35,11 +49,133 @@
                     <label for="">Código de Segurança</label>
                     <input type="text" name="card_cvv" id="" class="form-control">
                 </div>
+
+                <div class="col-md-12 installments form-group"></div>
             </div>
 
-            <button type="submit" class="btn btn-lg btn-success">Efetuar Pagamento</button>
+            <button type="submit" class="btn btn-lg btn-success processCheckout">Efetuar Pagamento</button>
         </form>
     </div>
 </div>
 
+@endsection
+
+@section('scripts')
+    <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous">
+    </script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        const sessionId = "{{session()->get('pagseguro_session_code')}}";
+
+        PagSeguroDirectPayment.setSessionId(sessionId);
+    </script>
+
+    <script>
+        let amountTransaction = '{{$cartItems}}';
+        let cardNumber = document.querySelector('input[name=card_number]');
+        let spanBrand = document.querySelector('span.brand');
+
+        cardNumber.addEventListener('keyup', function() {
+            if(cardNumber.value.length >= 6) {
+                PagSeguroDirectPayment.getBrand({
+                    cardBin: cardNumber.value.substr(0, 6),
+                    success: function(res) {
+                        
+                        let imgFlag = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${res.brand.name}.png">`;
+                        
+                        spanBrand.innerHTML = imgFlag;
+
+                        document.querySelector('input[name=card_brand]').value = res.brand.name;
+
+                        getInstallments(amountTransaction, res.brand.name);
+                    },
+                    error: function(err) {
+                        console.log(err);
+                    },
+                    complete: function(res) {
+                        console.log('Complete: ' + res);
+                    }
+                });
+            }
+        });
+
+        let submitButton = document.querySelector('button.processCheckout');
+
+        submitButton.addEventListener('click', function(event) {
+
+            event.preventDefault();
+
+
+            PagSeguroDirectPayment.createCardToken({
+                cardNumber: document.querySelector('input[name=card_number]').value,
+                brand: document.querySelector('input[name=card_brand]').value,
+                cvv: document.querySelector('input[name=card_cvv]').value,
+                expirationMonth: document.querySelector('input[name=card_month]').value,
+                expirationYear: document.querySelector('input[name=card_year]').value,
+                success: function(res) {
+                    processPayment(res.card.token);
+                }
+            });
+        });
+
+        function processPayment(token) {
+
+            let data = {
+                card_token: token,
+                hash: PagSeguroDirectPayment.getSenderHash(),
+                installment: document.querySelector('.select_installments').value,
+                card_name: document.querySelector('input[name=card_name]').value,
+                _token: '{{csrf_token()}}'
+            };
+
+
+            $.ajax({
+                type: 'POST',
+                url: "{{route('checkout.process')}}",
+                data: data,
+                dataType: 'json',
+                success: function(res) {
+                    toastr.success(res.data.message, 'Sucesso')
+                    window.location.href = '{{route("checkout.thanks")}}?order=' + res.data.message;
+                }
+            });
+        }
+
+        function getInstallments(amount, brand) {
+            PagSeguroDirectPayment.getInstallments({
+                amount: amount,
+                brand: brand,
+                maxInstallNoInterest: 0,
+                success: function(res) {
+                    let selectInstallments = drawSelectInstallments(res.installments[brand]);
+                    document.querySelector('div.installments').innerHTML = selectInstallments;
+                },
+                error: function(err) {
+                    console.log(err);
+                },
+                complete: function(res) {
+                    console.log(res);
+                }
+            });
+        }
+
+        function drawSelectInstallments(installments) {
+            let select = '<label>Opções de Parcelamento:</label>';
+
+            select += '<select class="form-control select_installments">';
+
+            for(let l of installments) {
+                select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de ${l.installmentAmount} - Total fica ${l.totalAmount}</option>`;
+            }
+
+
+            select += '</select>';
+
+            return select;
+        }
+    </script>
 @endsection
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -13,6 +13,7 @@
             margin-bottom: 40px;
         }
     </style>
+    @yield('stylesheets')
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 40px;">
@@ -75,5 +76,6 @@
     @include('flash::message')
     @yield('content')
 </div>
+@yield('scripts')
 </body>
 </html>

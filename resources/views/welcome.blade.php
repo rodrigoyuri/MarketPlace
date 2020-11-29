266812d05c630a50bd632ded8440d25b4e429d0e
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-29 18:07:44 -0300

    Exibição e compartilhamento de categories entre as views.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,4 +21,24 @@
         @if(($key + 1) % 3 == 0) </div><div class="row front"> @endif
     @endforeach
     </div>
+
+    <div class="row">
+        <div class="col-12">
+            <h2>Lojas em Destaque</h2>
+            <hr>
+        </div>
+        @foreach($stores as $store)
+            <div class="col-4">
+                
+                @if($store->logo)
+                    <img src="{{asset('storage/' . $store->logo)}}" alt="Logo da Loja {{$store->name}} " class="img-fluid">
+                @else 
+                    <img src="http://via.placeholder.com/450x100.png?text=logo" alt="Logo da Loja {{$store->name}} " class="img-fluid">
+                @endif
+
+                <h3>{{$store->name}}</h3>
+                <p>{{$store->description}}</p>
+            </div>
+        @endforeach
+    </div>
 @endsection

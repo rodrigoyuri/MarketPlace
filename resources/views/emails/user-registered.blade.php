46f4e70c2e0a0387105785ea0b2f5e3836e31e1f
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-02 15:53:16 -0300

    correção de variavel para acesso de atributo.

--- resources/views/emails/user-registered.blade.php
+++ resources/views/emails/user-registered.blade.php
@@ -1,10 +1,10 @@
-<h1>Olá, {{$this->name}}, tudo bem? Espero que sim!</h1>
+<h1>Olá, {{$user->name}}, tudo bem? Espero que sim!</h1>
 
 <h3>Obrigado por sua inscrição</h3>
 
 <p>
     Faça bom proveito e excelentes compras em nosso Marketplace!
-    Seu Email de cadastro é: <strong>{{$this->email}}</strong>
+    Seu Email de cadastro é: <strong>{{$user->email}}</strong>
 </p>
 <hr>
 Email envio em {{date('d/m/y H:i:s')}}.

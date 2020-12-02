ad37b5b4f9e915c9dbf44a453e64a8ac6c14d33f
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-01 21:10:32 -0300

    Instalação de dependencias e build do laravel-mix.

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Marketplace L6</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <style>
         .front.row {
             margin-bottom: 40px;

3432ad712ad840eba27a8e8a8f2b5dcd59eb2c5f
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-03 20:12:33 -0300

    Adição de mascara no input do create e edit products e remoção de links do jquery.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -44,7 +44,7 @@
 
     <div class="form-group">
         <label for="">Preço</label>
-        <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}">
+        <input type="text" id="price" name="price" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}">
 
         @error('price')
             <div class="invalid-feedback">
@@ -81,3 +81,17 @@
 
 @endsection
 
+@section('scripts')
+
+<script src="https://cdn.rawgit.com/plentz/jquery-maskmoney/master/dist/jquery.maskMoney.min.js"></script>
+
+<script>
+    $('#price').maskMoney({
+        prefix: 'R$',
+        allowNegative: false,
+        thousands: '.',
+        decimal: ','
+    });
+</script>
+
+@endsection
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -45,7 +45,7 @@
 
     <div class="form-group">
         <label for="">Preço</label>
-        <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{$product->price}}">
+        <input type="text" id="price" name="price" class="form-control @error('price') is-invalid @enderror" value="{{$product->price}}">
 
         @error('price')
             <div class="invalid-feedback">
@@ -98,3 +98,17 @@
 
 @endsection
 
+@section('scripts')
+
+<script src="https://cdn.rawgit.com/plentz/jquery-maskmoney/master/dist/jquery.maskMoney.min.js"></script>
+
+<script>
+    $('#price').maskMoney({
+        prefix: 'R$',
+        allowNegative: false,
+        thousands: '.',
+        decimal: ','
+    });
+</script>
+
+@endsection

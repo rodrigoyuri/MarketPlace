c2dfa59c3bf7e8228024cb418ca5d5b5b88ab549
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-03 20:41:30 -0300

    Formatação do input valor input price para inserir no banco.

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -61,6 +61,8 @@ class ProductController extends Controller
         $data = $request->all();
         $categories = $request->get('categories', null);
 
+        $data['price'] = formatPriceToDatabase($data['price']);
+
         $store = auth()->user()->store;
         $product = $store->products()->create($data);
 
@@ -111,6 +113,8 @@ class ProductController extends Controller
     {
         $data = $request->all();
         $categories = $request->get('categories', null);
+
+        $data['price'] = formatPriceToDatabase($data['price']);
 
         $product = $this->product->find($id);
         $product->update($data);

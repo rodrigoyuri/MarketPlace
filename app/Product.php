17c937691d39b2f18addb35a58a9010e29c0967f
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-04 10:50:43 -0300

    metodo para exibição de imagem criado e utilizado e js para movimentação de imagem na view single.

--- app/Product.php
+++ app/Product.php
@@ -19,6 +19,11 @@ class Product extends Model
                             ->saveSlugsTo('slug');
     }
 
+    public function getThumbAttribute()
+    {
+        return $this->photos->first()->image;
+    }
+
     public function store()
     {
         return $this->belongsTo(Store::class);

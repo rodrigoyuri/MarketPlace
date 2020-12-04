03bdf2459cbecea673e2ecad197c9e0ea863846c
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-03 22:02:16 -0300

    Atualizando o pagseguro_status pela Notificação do Pagseguro.

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Payment\PagSeguro\CreditCard;
+use App\Payment\PagSeguro\Notification;
 use App\Store;
+use App\UserOrder;
 use Illuminate\Http\Request;
 use Ramsey\Uuid\Uuid;
 
@@ -85,6 +87,37 @@ class CheckoutController extends Controller
     public function thanks()
     {
         return view('thanks');
+    }
+
+    public function notification()
+    {
+        try{
+
+            $notification = new Notification();
+
+            $notification = $notification->getTransaction();
+
+            //Atualizar o pedido do usuário
+            $userOrder = UserOrder::whereReference($notification->getReference());
+            $userOrder->update([
+                'pagseguro_status' => $notification->getStatus()
+            ]);
+
+            if($notification->getStatus() == 3) {
+                // Liberar o pedido do usuário..., atualizar o status do pedido para em separação
+                // Notificar o usuário que o pedido foi pago...
+                // Notificar o laja da confirmação do pedido...
+            }
+
+            return response()->json([], 204);
+
+        }catch(\Exception $e) {
+
+            $message = env('APP_DEBUG') ? $e->getMessage() : '';
+
+            return response()->json(['error' => $message], 500);
+
+        }
     }
 
     private function makePagSeguroSession()

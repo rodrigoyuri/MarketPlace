64576308a4b22c8293283a4edf0551a77e8d5655
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-04 11:04:56 -0300

    Function para criar slug manualmente.

--- app/Product.php
+++ app/Product.php
@@ -3,25 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Sluggable\HasSlug;
-use Spatie\Sluggable\SlugOptions;
+// use Spatie\Sluggable\HasSlug;
+// use Spatie\Sluggable\SlugOptions;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
-    use HasSlug;
+    // use HasSlug;
     
     protected $fillable = ['name', 'description', 'body', 'price', 'slug'];
 
-    public function getSlugOptions()
-    {
-        return SlugOptions::create()
-                            ->generateSlugsFrom('name')
-                            ->saveSlugsTo('slug');
-    }
+    // public function getSlugOptions()
+    // {
+    //     return SlugOptions::create()
+    //                         ->generateSlugsFrom('name')
+    //                         ->saveSlugsTo('slug');
+    // }
 
     public function getThumbAttribute()
     {
         return $this->photos->first()->image;
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+
+        $this->attributes['slug'] = Str::slug($value);
     }
 
     public function store()
